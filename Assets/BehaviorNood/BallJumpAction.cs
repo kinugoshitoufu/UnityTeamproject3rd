3ad7e4824fec922949ed66c21c9b599d3f2a21cf
MAETEL\2247041 <2247041@ecc.ac.jp>
2026-01-08 11:24:21 +0900

    象のアルゴリズム(製作中)

---
+++ Assets/BehaviorNood/BallJumpAction.cs
@@ -0,0 +1,26 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "BallJump", story: "Jump and Ball", category: "Action", id: "ea65e7cf1a19b8fd943bbb92f4389187")]
+public partial class BallJumpAction : Action
+{
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
